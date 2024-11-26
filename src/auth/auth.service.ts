1a41a2ef56126b384e1f639ec947fbeeabe7f725
Al.Ahiyat <islam2_14@mail.ru>
2024-11-27 02:38:55 +0300

    bagfix 0.2

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -5,32 +5,37 @@ import { UserLoginDto } from "./dto/user-login.dto";
 import * as bcrypt from "bcrypt";
 import { AuthUserResponse } from "./response/auth.response";
 import { TokenService } from "src/token/token.service";
+import e from "express";
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly userService: UsersService,
-    private readonly tokenService: TokenService
-  ) {}
+    constructor(
+        private readonly userService: UsersService,
+        private readonly tokenService: TokenService
+    ) {}
 
-  async registerUser(dto: CreateUserDto): Promise<CreateUserDto> {
-    const existUser = await this.userService.findUserByEmail(dto.email)
-    if(existUser) throw new HttpException('Данный пользователь уже в системе',HttpStatus.BAD_REQUEST);
+    async registerUser(dto: CreateUserDto): Promise<CreateUserDto> {
+        try {
+            const existUser = await this.userService.findUserByEmail(dto.email);
+            if (existUser) throw new HttpException("Данный пользователь уже в системе", HttpStatus.BAD_REQUEST);
+            return this.userService.createUser(dto);
+        } catch (e) {
+            throw new Error(e);
+        }
+    }
 
-    return this.userService.createUser(dto)
-  }
+    async loginUser(dto: UserLoginDto): Promise<AuthUserResponse> {
+        try {
+            const existUser = await this.userService.findUserByEmail(dto.email);
+            if (!existUser) throw new HttpException("Неверный пароль или логин", HttpStatus.BAD_REQUEST);
 
-  async loginUser(dto: UserLoginDto): Promise<AuthUserResponse>{
-    const existUser = await this.userService.findUserByEmail(dto.email)
-    if(!existUser) throw new HttpException('Неверный пароль или логин',HttpStatus.BAD_REQUEST);
-    
-    const validatePassword = await bcrypt.compare(dto.password, existUser.password)
-    if(!validatePassword) throw new HttpException('Неверный пароль или логин',HttpStatus.BAD_REQUEST);
-    const user = await this.userService.publicUser(dto.email)
-    const token = await this.tokenService.generateJwtToken(user)
-    return {user, token}
-  }
-
-  
+            const validatePassword = await bcrypt.compare(dto.password, existUser.password);
+            if (!validatePassword) throw new HttpException("Неверный пароль или логин", HttpStatus.BAD_REQUEST);
+            const user = await this.userService.publicUser(dto.email);
+            const token = await this.tokenService.generateJwtToken(user);
+            return { user, token };
+        } catch (e) {
+            throw new Error(e);
+        }
+    }
 }
-
-// prettier-ignore
